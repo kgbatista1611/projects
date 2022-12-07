35a6ba00c2586e04792e3773387c724e0022adbd
kgbatista1611 <117448985+kgbatista1611@users.noreply.github.com>
2022-12-07 16:08:44 -0300

    Add files via upload

---
+++ new01.py
@@ -0,0 +1,32 @@
+import random
+
+fruit = ['banana', 'maçã', 'morango']
+x, y, z = fruit
+s = 35e10
+f = -47e-22
+d = 3+5j
+k = random.randrange(3,10)
+
+# Teste de condicional
+txt = '''Hello, my name is Kleber Batista. I'm from Brazil.
+     Me like really study, walk, drink coffee, see the river in my city'''
+if 'Argentina' not in txt:
+    print("\n No, not 'Argentina' in 'TXT'! \n TXT=", txt, "\n")
+
+new_test = "Hello, sciling test"
+print(new_test[7:15], "\n \n")
+print(new_test[:15], "\n \n")
+
+print(k, "\n")
+print(d, "\n")
+print(f, "\n")
+print(x, "\n")
+print(s, "\n")
+print(z, "\n")
+
+
+
+
+
+
+
